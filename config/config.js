90d3d71de36047de24e2095366748d7564fdc028
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2020-03-04 20:37:03 +0800

    feat: 更新一版文档

--- config/config.js
+++ config/config.js
@@ -3,17 +3,5 @@ export default {
   description: "Java 单元测试作战手册",
   logo: "https://f2.xiyunerp.com/xy-logo.png",
   mode: "docs",
-  // menus: {
-  //   "/guide": [
-  //     {
-  //       title: "介绍",
-  //       children: ["guide/00-introduce/index"]
-  //     },
-  //   ]
-  // },
-  navs: [
-    { title: "GitHub", path: "https://github.com/umijs/dumi" },
-    { title: "更新日志", path: "https://github.com/umijs/dumi/releases" }
-  ],
-  extraBabelPlugins: []
+  publicPath: "/java-unit-docs/",
 };
