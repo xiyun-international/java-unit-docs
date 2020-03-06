0914cd82251ee75adae04a9ee6c0cc598896983b
zyq <zhaoyongqiang@xiyun.com.cn>
2020-03-06 20:21:57 +0800

    添加demo源码

---
+++ src/test/java/com/xiyun/xiyuntest/web/XiyunTestWebApplicationTests.java
@@ -0,0 +1,24 @@
+package test.java.com.xiyun.xiyuntest.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class XiyunTestWebApplicationTests {
+
+    @Test
+    void firstExample() {
+        //创建模拟对象
+        List mockedList = mock(List.class);
+        //使用模拟对象
+        mockedList.add("one");
+        mockedList.clear();
+        //验证
+        verify(mockedList).add("one");
+        verify(mockedList).clear();
+    }
+}
