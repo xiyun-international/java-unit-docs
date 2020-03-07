d1eba64592fd03ef000587e188dc3d8d056e3c6e
zyq <zhaoyongqiang@xiyun.com.cn>
2020-03-07 16:46:02 +0800

    修改源码,增加输出响应信息

--- source/xiyun-test-web/src/test/java/com/xiyun/xiyun/test/web/XiyunTestWebApplicationTests.java
+++ source/xiyun-test-web/src/test/java/com/xiyun/xiyun/test/web/XiyunTestWebApplicationTests.java
@@ -12,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
@@ -31,13 +32,14 @@ class XiyunTestWebApplicationTests {
         MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.post("/user/login")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(JSONObject.toJSONString(userDO)))
+                .andDo(MockMvcResultHandlers.print())
                 .andReturn()
                 .getResponse();
         //验证http状态码
         Assertions.assertNotEquals(MockMvcResultMatchers.status().isOk(), response.getStatus());
         CallResult callResult = JSONObject.parseObject(response.getContentAsString(), CallResult.class);
         //验证业务状态码
-        Assertions.assertNotEquals(callResult.getCode(), CallResult.RETURN_STATUS_OK);
+        Assertions.assertEquals(callResult.getCode(), CallResult.RETURN_STATUS_OK);
 
     }
 
