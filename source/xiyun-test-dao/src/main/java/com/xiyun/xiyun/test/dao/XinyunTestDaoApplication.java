936719396c0d3c49d16f0ed73e97e1c649b26e59
zyq <zhaoyongqiang@xiyun.com.cn>
2020-03-07 15:26:37 +0800

    添加源码，增加dao层测试代码demo

---
+++ source/xiyun-test-dao/src/main/java/com/xiyun/xiyun/test/dao/XinyunTestDaoApplication.java
@@ -0,0 +1,13 @@
+package com.xiyun.xiyun.test.dao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XinyunTestDaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XinyunTestDaoApplication.class, args);
+    }
+
+}
