e62eff41ded564ad2d14fd2d08eb1b004977035a
zyq <zhaoyongqiang@xiyun.com.cn>
2020-03-07 16:40:55 +0800

    修改源码,增加输出响应信息

--- source/xiyun-test-web/src/test/java/com/xiyun/xiyun/test/web/XiyunTestWebApplicationTests.java
+++ source/xiyun-test-web/src/test/java/com/xiyun/xiyun/test/web/XiyunTestWebApplicationTests.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
