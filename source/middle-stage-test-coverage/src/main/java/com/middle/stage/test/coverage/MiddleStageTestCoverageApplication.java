3c176a7c8bbe744dfe7ccfed0578dc96f36a8ff8
zyq <zhaoyongqiang@xiyun.com.cn>
2020-03-10 15:56:52 +0800

    更新源码

--- source/middle-stage-test-coverage/src/main/java/com/middle/stage/test/coverage/MiddleStageTestCoverageApplication.java
+++ source/middle-stage-test-coverage/src/main/java/com/middle/stage/test/coverage/MiddleStageTestCoverageApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.middle.stage.test.impl.mapper")
+@MapperScan(basePackages = "com.middle.stage.test.coverage.mapper")
 public class MiddleStageTestCoverageApplication {
 
     public static void main(String[] args) {
