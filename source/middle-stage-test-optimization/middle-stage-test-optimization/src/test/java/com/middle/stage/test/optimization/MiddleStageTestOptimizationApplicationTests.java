7391c266d87c6cb0591d1835dce4f6268e90ba46
zyq <zhaoyongqiang@xiyun.com.cn>
2020-03-12 17:50:37 +0800

    提交未添加代码

---
+++ source/middle-stage-test-optimization/middle-stage-test-optimization/src/test/java/com/middle/stage/test/optimization/MiddleStageTestOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.middle.stage.test.optimization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddleStageTestOptimizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ source/middle-stage-test-optimization/src/main/java/com/middle/stage/test/optimization/MiddleStageTestOptimizationApplication.java
@@ -0,0 +1,13 @@
+package com.middle.stage.test.optimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddleStageTestOptimizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddleStageTestOptimizationApplication.class, args);
+    }
+
+}
